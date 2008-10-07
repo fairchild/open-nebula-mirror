5688d371e42bc3c004c79b5070e7a9ca147f9bcc
jfontan <jfontan@3034c82b-c49b-4eb3-8279-a7acafdc01c0>
2008-10-07 17:48:29 +0000

    TMScript: Added documentation
    git-svn-id: http://svn.opennebula.org/one/branches/DEVELOPMENT@130 3034c82b-c49b-4eb3-8279-a7acafdc01c0

--- TransferManager/src/tm_mad/TMScript.rb
+++ TransferManager/src/tm_mad/TMScript.rb
@@ -2,25 +2,64 @@
 require 'pp'
 require 'open3'
 
+=begin rdoc
+
+TMPlugin holds the name of the scripts that will be used for each
+TransferManager script command. It is basically a hash where keys
+are the names of the commands (uppercase) and contain the path of
+the script that will be executed for that command.
+
+It also contains some methods to execute the scripts, get the output
+of the script (success/failure, error and log messages). The protocol
+for scripts to do so is as follows:
+
+* Log messages will be sent to STDOUT
+* The script will return 0 if it succeded or any other value
+  if there was a failure
+* In case of failure the cause of the error will be written to STDERR
+  wrapped by start and end marks as follows:
+
+    ERROR MESSAGE --8<------
+    error message for the failure
+    ERROR MESSAGE ------>8--
+
+=end
 class TMPlugin < Hash
+    
+    # Sets the script path for the specific +command+
     def set(command, script)
         self[command]=script
     end
     
+    # Executes the script associated with the +command+ using
+    # specified arguments.
+    #
+    # Returns:
+    # * It will return +nil+ if the +command+ is not defined.
+    # * String with stderr output of the string (exit code and
+    #   error message in case of failure)
+    #
+    # Note: the exit code will be written like this:
+    #   ExitCode: 0
     def execute(command, *args)
         # Command is not defined
         return nil if !self[command]
         
+        # Generates the line to call the script with all the
+        # arguments provided.
         cmd=[self[command], *args].join(" ")
         exec_local_command(cmd)
     end
     
     private
     
+    # Executes the command, get its exit code and logs every line that
+    # comes from stdout. Returns whatever came from stderr.
     def exec_local_command(command)
         cmd="#{command} ; echo ExitCode: $? 1>&2"
         stderr=""
         std=Open3.popen3(cmd) {|stdin, stdout, stderr_|
+            # TODO: this should be sent to ONE and not to STDERR
             while !stdout.eof?
                 STDERR.puts stdout.readline
                 STDERR.flush
@@ -30,27 +69,15 @@ class TMPlugin < Hash
     end
 end
 
+# This class will parse and execute TransferManager scripts.
 class TMScript
     attr_accessor :lines
     
     def initialize(script_text)
         @lines=Array.new
-        parse(script_text)
+        parse_script(script_text)
     end
-    
-    def parse(script_text)
-        script_text.each_line {|line|
-            # skip if the line is commented
-            next if line.match(/^\s*#/)
-            # skip if the line is empty
-            next if line.match(/^\s*$/)
-            
-            command=line.split(" ")
-            command[0].upcase!
-            @lines<< command
-        }
-    end
-    
+        
     def execute(plugin)
         result=@lines.each {|line|
             res=plugin.execute(*line)
@@ -69,6 +96,21 @@ class TMScript
         
         pp result
         
+    end
+    
+    private
+    
+    def parse_script(script_text)
+        script_text.each_line {|line|
+            # skip if the line is commented
+            next if line.match(/^\s*#/)
+            # skip if the line is empty
+            next if line.match(/^\s*$/)
+            
+            command=line.split(" ")
+            command[0].upcase!
+            @lines<< command
+        }
     end
     
     def parse_output(err)
