e865ce899833c0703594005753e8961f1ff8e734
jfontan <jfontan@3034c82b-c49b-4eb3-8279-a7acafdc01c0>
2008-10-20 14:04:33 +0000

    tm_mad: added timeout function to execute commands
    git-svn-id: http://svn.opennebula.org/one/branches/DEVELOPMENT@163 3034c82b-c49b-4eb3-8279-a7acafdc01c0

--- TransferManager/src/tm_mad/tm_common.sh
+++ TransferManager/src/tm_mad/tm_common.sh
@@ -54,3 +54,43 @@ function exec_and_log
     fi
     log "Executed \"$1\"."
 }
+
+# Like exec_and_log but the first argument is the number of seconds
+# before here is timeout and kills the command
+#
+# NOTE: if the command is killed because a timeout the exit code
+# will be 143 = 128+15 (SIGHUP)
+function timeout_exec_and_log
+{
+    TIMEOUT=$1
+    shift
+
+    CMD="$1"
+
+    exec_and_log "$CMD" &
+    CMD_PID=$!
+
+    # timeout process
+    (
+        sleep $TIMEOUT
+        kill $CMD_PID 2>/dev/null
+        log_error "Timeout executing $CMD"
+        error_message "Timeout executing $CMD"
+        exit -1
+    ) &
+    TIMEOUT_PID=$!
+
+    # stopts the exution until the command finalizes
+    wait $CMD_PID 2>/dev/null
+    CMD_CODE=$?
+    
+    # if the script reaches here the command finished before it
+    # consumes timeout seconds so we can kill timeout process
+    kill $TIMEOUT_PID 2>/dev/null 1>/dev/null
+    wait $TIMEOUT_PID 2>/dev/null
+
+    # checks the exit code of the command and exits if it is not 0
+    if [ "x$CMD_CODE" != "x0" ]; then
+        exit $CMD_CODE
+    fi
+}
