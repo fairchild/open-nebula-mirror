d963f97c6a2db6b9e30ceb2d3c590d8f5fb208de
ruben <ruben@3034c82b-c49b-4eb3-8279-a7acafdc01c0>
2009-06-09 16:48:04 +0000

    Work in the Networks..
    git-svn-id: http://svn.opennebula.org/one/branches/DEVELOPMENT@564 3034c82b-c49b-4eb3-8279-a7acafdc01c0

--- OpenNebulaXML/src/client/ruby/lib/OpenNebula/VirtualNetwork.rb
+++ OpenNebulaXML/src/client/ruby/lib/OpenNebula/VirtualNetwork.rb
@@ -2,30 +2,103 @@
 module OpenNebula
 
     class VirtualNetworkPool
-
+        # ---------------------------------------------------------------------
+        # Constants and Class attribute accessors
+        # ---------------------------------------------------------------------
         VN_POOL_METHODS = {
             :info => "vnpool.info"
         }
 
-        def initialize(user_id=0)
+        attr_reader :pool_xml
+
+        # ---------------------------------------------------------------------
+        # Class constructor
+        # ---------------------------------------------------------------------
+        def initialize(user_id=0, secret=nil)
             @user_id  = user_id
             @pool_xml = nil
 
-            @client   = Client.new;
-            #TODO Check for Errors
+            @client   = OpenNebula::Client.new(secret); #TODO Check for Errors
         end
 
-        def get()
-            @pool_xml = @client.call(VN_POOL_METHODS[:info],user_id)
+        # ---------------------------------------------------------------------
+        # XML-RPC Methods for the Virtual Network Object
+        # ---------------------------------------------------------------------
+        def info()
+            rc = @client.call(VN_POOL_METHODS[:info],@user_id)
 
-            if is_error?(@pool_xml)
-                error     = @pool_xml.clone
+            if OpenNebula.is_error?(rc)
                 @pool_xml = nil
             else
-                error = nil
+                @pool_xml = rc[0];
+                rc = nil
             end
 
-            error
+            rc
         end
     end
+    
+    ###########################################################################
+    ###########################################################################
+
+    class VirtualNetwork
+        # ---------------------------------------------------------------------
+        # Constants and Class attribute accessors
+        # ---------------------------------------------------------------------
+        VN_METHODS = {
+            :info     => "vn.info",
+            :allocate => "vn.allocate",
+            :delete   => "vn.delete"
+        }
+
+        attr_reader :vn_xml
+
+        # ---------------------------------------------------------------------
+        # Class constructor
+        # ---------------------------------------------------------------------
+        def initialize(vn_id=nil, secret=nil)
+            @vn_id  = vn_id
+            @vn_xml = nil
+
+            @client = OpenNebula::Client.new(secret)
+        end
+
+        # ---------------------------------------------------------------------
+        # XML-RPC Methods for the Virtual Network Object
+        # ---------------------------------------------------------------------
+        def info()
+            return Error.new('Virtual Network ID not defined') if !@vn_id
+            rc = @client.call(VN_METHODS[:info],@vn_id)
+
+            if OpenNebula.is_error?(rc)
+                @vn_xml = nil
+            else
+                @vn_xml = rc[0]
+                rc = nil
+            end
+
+            rc
+        end
+
+        def allocate(description)
+            rc = @client.call(VN_METHODS[:allocate],description)
+
+            if !OpenNebula.is_error?(rc)
+                @vn_id = rc[0]
+                rc = nil
+            end
+
+            return rc
+        end
+
+        def delete()
+            return Error.new('Virtual Network ID not defined') if !@vn_id
+
+            rc = @client.call(VN_METHODS[:delete],@vn_id)
+            rc = nil if !OpenNebula.is_error?(rc)
+
+            return rc
+        end
+    end
+
 end
