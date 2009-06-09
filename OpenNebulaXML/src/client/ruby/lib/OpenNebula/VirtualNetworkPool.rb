6ecef600cc9ec92d1851239f82011bc6ae436bf5
ruben <ruben@3034c82b-c49b-4eb3-8279-a7acafdc01c0>
2009-06-09 20:00:23 +0000

    New VirtualNetworkPool and test for base Pool and PoolElement classes
    git-svn-id: http://svn.opennebula.org/one/branches/DEVELOPMENT@565 3034c82b-c49b-4eb3-8279-a7acafdc01c0

---
+++ OpenNebulaXML/src/client/ruby/lib/OpenNebula/VirtualNetworkPool.rb
@@ -0,0 +1,140 @@
+
+module OpenNebula
+
+    # The Pool class represents a generic OpenNebula Pool in XML format
+    # and provides the basic functionality to handle the Pool elements
+    class Pool
+        include Enumerable
+
+        def initialize(pool,element)
+            @pool_name    = pool.upcase
+            @element_name = element.upcase
+        end
+
+        # Iterates over every VirtualNetwork and calls the block with a
+        # VirtualNetworkPoolNode
+        def each
+            if @xml
+                doc=REXML::Document.new(@xml)
+                doc.elements.each("/#{@pool_name}/#{@element_name}") {|pelem|
+                    yield OpenNebula::PoolNode.new(pelem)
+                }
+            end
+        end
+    end
+    
+    # The PoolElement Class represents a generic element of a Pool in
+    # XML format
+    class PoolElement
+        # +node+ is a REXML element that represents the Pool element 
+        def initialize(node)
+            @xml = node
+        end
+
+        # Extract an element from the node:
+        #
+        #   ['VID'] # gets VM id
+        #   ['HISTORY/HOSTNAME'] # get the hostname from the history
+        def [](key)
+            @xml.elements[key.to_s.upcase].text
+        end
+    end
+
+    class VirtualNetworkPool < Pool
+        # ---------------------------------------------------------------------
+        # Constants and Class attribute accessors
+        # ---------------------------------------------------------------------
+
+        VN_POOL_METHODS = {
+            :info => "vnpool.info"
+        }
+
+        attr_reader :xml
+
+        # ---------------------------------------------------------------------
+        # Class constructor
+        # ---------------------------------------------------------------------
+        
+        # +secret+ is the authentication token used with the OpenNebula core
+        # +user_id+ is to refer to a Pool with VirtualNetworks from that user
+        def initialize(user_id=0, secret=nil)
+            @user_id  = user_id
+            @xml      = nil
+
+            @client = OpenNebula::Client.new(secret); 
+        end
+
+        # ---------------------------------------------------------------------
+        # XML-RPC Methods for the Virtual Network Object
+        # ---------------------------------------------------------------------
+
+        def info()
+            rc = @client.call(VN_POOL_METHODS[:info],@user_id)
+
+            if !OpenNebula.is_error?(rc)
+                @xml = rc;
+                rc   = nil
+            end
+            
+            return rc
+        end
+    end
+
+    class VirtualNetwork < PoolElement
+        # ---------------------------------------------------------------------
+        # Constants and Class attribute accessors
+        # ---------------------------------------------------------------------
+        VN_METHODS = {
+            :info     => "vn.info",
+            :allocate => "vn.allocate",
+            :delete   => "vn.delete"
+        }
+
+        attr_reader :vn_xml
+
+        # ---------------------------------------------------------------------
+        # Class constructor
+        # ---------------------------------------------------------------------
+        def initialize(vn_id=nil, secret=nil)
+            @vn_id = vn_id
+            @xml   = nil
+
+            @client = OpenNebula::Client.new(secret)
+        end
+
+        # ---------------------------------------------------------------------
+        # XML-RPC Methods for the Virtual Network Object
+        # ---------------------------------------------------------------------
+        def info()
+            return Error.new('Virtual Network ID not defined') if !@vn_id
+            rc = @client.call(VN_METHODS[:info],@vn_id)
+
+            if !OpenNebula.is_error?(rc)
+                @xml = rc
+                rc   = nil
+            end
+
+            return rc
+        end
+
+        def allocate(description)
+            rc = @client.call(VN_METHODS[:allocate],description)
+
+            if !OpenNebula.is_error?(rc)
+                @vn_id = rc
+                rc     = nil
+            end
+
+            return rc
+        end
+
+        def delete()
+            return Error.new('Virtual Network ID not defined') if !@vn_id
+
+            rc = @client.call(VN_METHODS[:delete],@vn_id)
+            rc = nil if !OpenNebula.is_error?(rc)
+
+            return rc
+        end
+    end
+end
